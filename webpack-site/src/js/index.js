0872f82da2c2dd3957bc313d6dc4374df40969a2
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2018-10-28 00:44:55 -0300

    link frontend to chat

--- webpack-site/src/js/index.js
+++ webpack-site/src/js/index.js
@@ -1,18 +1,51 @@
 import '../css/index.scss'
 
-addMessage('oi')
-
 const messageForm = document.querySelector('.message-form')
 
 messageForm.onsubmit = e => {
   e.preventDefault()
   const input =  messageForm.querySelector('input')
   const text = input.value
-  input.value = ''
+  
+  if(text == '') return
 
-  if(text != '') addMessage(text)
+  input.value = ''
+  addMessage(text, { isCurrentUser: true })
+  socket.send(text)
 }
 
+
+
+
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:3000')
+
+// Connection opened
+socket.onopen = function (event) {
+  console.log("connection opened")
+}
+
+// Listen for messages
+socket.onmessage = function(event) {
+  console.log('message from server:', event.data)
+  addMessage(event.data, { isCurrentUser: false })
+}
+
+
+
+function addMessage(text, opt) {
+  const { isCurrentUser } = opt
+
+  const message = createElement('div', text)
+
+  const classes = isCurrentUser ? 'current-user-message align-self-flex-end' :
+                                  'other-user-message align-self-flex-start'
+
+  message.className = classes
+
+  const messages = document.querySelector('.messages')
+  messages.appendChild(message)
+}
 
 function createElement(elementName, content) {
   const element = document.createElement(elementName)
@@ -20,15 +53,4 @@ function createElement(elementName, content) {
   element.appendChild(textNode)
 
   return element
-}
-
-function createMessage(text) {
-  const element = createElement('div', text)
-  element.className = "current-user-message align-self-flex-end"
-  return element
-}
-
-function addMessage(text) {
-  const messages = document.querySelector('.messages')
-  messages.appendChild(createMessage(text))
 }
